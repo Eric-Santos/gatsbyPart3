066ccc9e7ea96c2e889aaac66ea3540ec26e4d38
eric <Eric355402@yahoo.com>
2018-06-03 14:02:25 -0500

    completed part 3 tutorial

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+	<div>
+		<h1>Id love to talk! Email me at the address below</h1>
+		<p>
+			<a href="mailto:eric355402@yahoo.com">eric355402@yahoo.com</a>
+		</p>
+	</div>
+);
